8175ba8954ca620054f48485dd1ddd4d5a49bfad
Daniel <dn2828@gmail.com>
2022-01-25 16:50:05 +0200

    drop redundant lines

--- src/UDPSendMessage.java
+++ src/UDPSendMessage.java
@@ -5,25 +5,13 @@ import java.net.InetAddress;
 
 
 public class UDPSendMessage {
-    private final String _fromRouterName;
-    private final String _toIP;
-    private final int _toPort;
-    private final String _message;
-
     public UDPSendMessage(String fromRouterName, String toIP, int toPort, String message) {
-        _fromRouterName = fromRouterName;
-        _toIP = toIP;
-        _toPort = toPort;
-        _message = message;
-
-        // Step 1:Create the socket object for
-        // carrying the data.
         try {
             DatagramSocket ds = new DatagramSocket();
 
             InetAddress ip = InetAddress.getLocalHost();
             byte[] buf = null;
-            buf = _message.getBytes();
+            buf = message.getBytes();
             DatagramPacket DpSend =
                     new DatagramPacket(buf, buf.length, InetAddress.getByName(toIP), toPort);
             ds.send(DpSend);
